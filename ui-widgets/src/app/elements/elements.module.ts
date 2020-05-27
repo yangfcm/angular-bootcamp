388d8e5fa109647c77e849f3b7bbd1f60d00a81e
Fan Yang <yangfcm16@gmail.com>
2020-05-27 23:43:04 +0930

    ui-widgets: make route work

--- ui-widgets/src/app/elements/elements.module.ts
+++ ui-widgets/src/app/elements/elements.module.ts
@@ -7,6 +7,6 @@ import { ElementsHomeComponent } from './elements-home/elements-home.component';
 @NgModule({
   declarations: [ElementsHomeComponent], // The components, pipes, directives created in this module
   imports: [CommonModule, ElementsRoutingModule], // The modules that this module depends on
-  exports: [ElementsHomeComponent], // The components, pipes and directives that are exposed to other modules
+  exports: [], // The components, pipes and directives that are exposed to other modules
 })
 export class ElementsModule {}
