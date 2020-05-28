5599d3986551cd6cc46c1cfd5b47e830897d8228
Fan Yang <yangfcm16@gmail.com>
2020-05-28 10:04:57 +0930

    implement lazy loading

--- ui-widgets/src/app/app.module.ts
+++ ui-widgets/src/app/app.module.ts
@@ -3,14 +3,14 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { ElementsModule } from './elements/elements.module';
-import { CollectionsModule } from './collections/collections.module';
+// import { ElementsModule } from './elements/elements.module';
+// import { CollectionsModule } from './collections/collections.module';
 import { HomeComponent } from './home/home.component';
 import { NotFoundComponent } from './not-found/not-found.component';
 
 @NgModule({
   declarations: [AppComponent, HomeComponent, NotFoundComponent],
-  imports: [BrowserModule, ElementsModule, CollectionsModule, AppRoutingModule], // Order here is important!
+  imports: [BrowserModule, AppRoutingModule], // Order here is important!
   providers: [],
   bootstrap: [AppComponent],
 })
