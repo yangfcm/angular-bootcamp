3b4636a4878d88ee43f6e737882ea9834552b421
Fan Yang <yangfcm16@gmail.com>
2020-05-28 11:33:12 +0930

    ui-widgets: add table component

--- ui-widgets/src/app/collections/collections-home/collections-home.component.ts
+++ ui-widgets/src/app/collections/collections-home/collections-home.component.ts
@@ -3,13 +3,23 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-collections-home',
   templateUrl: './collections-home.component.html',
-  styleUrls: ['./collections-home.component.css']
+  styleUrls: ['./collections-home.component.css'],
 })
 export class CollectionsHomeComponent implements OnInit {
+  data = [
+    { name: 'James', age: 25, job: 'Designer' },
+    { name: 'Jill', age: 26, job: 'Engineer' },
+    { name: 'Ali', age: 30, job: 'Developer' },
+    { name: 'Alex', age: 29, job: 'Developer' },
+    { name: 'Bell', age: 30, job: 'Manager' },
+  ];
+  headers = [
+    { key: 'name', label: 'Name' },
+    { key: 'age', label: 'Age' },
+    { key: 'job', label: 'Job' },
+  ];
 
-  constructor() { }
+  constructor() {}
 
-  ngOnInit(): void {
-  }
-
+  ngOnInit(): void {}
 }
