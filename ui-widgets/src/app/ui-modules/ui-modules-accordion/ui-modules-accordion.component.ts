6e626fdd3ccf4e1352dc7228cabbc298667cf89e
Fan Yang <yangfcm16@gmail.com>
2020-05-29 10:44:40 +0930

    ui-widgets: add accordion

--- ui-widgets/src/app/ui-modules/ui-modules-accordion/ui-modules-accordion.component.ts
+++ ui-widgets/src/app/ui-modules/ui-modules-accordion/ui-modules-accordion.component.ts
@@ -1,15 +1,22 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
 
 @Component({
   selector: 'app-ui-modules-accordion',
   templateUrl: './ui-modules-accordion.component.html',
-  styleUrls: ['./ui-modules-accordion.component.css']
+  styleUrls: ['./ui-modules-accordion.component.css'],
 })
 export class UiModulesAccordionComponent implements OnInit {
+  @Input() items = [];
+  openedItemIndex = 0;
+  constructor() {}
 
-  constructor() { }
+  ngOnInit(): void {}
 
-  ngOnInit(): void {
+  onToggleContent(index: number) {
+    if (index === this.openedItemIndex) {
+      this.openedItemIndex = -1;
+    } else {
+      this.openedItemIndex = index;
+    }
   }
-
 }
