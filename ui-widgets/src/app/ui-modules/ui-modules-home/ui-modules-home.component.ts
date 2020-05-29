28a7b03c40e678772721bf974aa3345192ec9e11
Fan Yang <yangfcm16@gmail.com>
2020-05-29 10:25:40 +0930

    ui-widgets: add modal component

--- ui-widgets/src/app/ui-modules/ui-modules-home/ui-modules-home.component.ts
+++ ui-widgets/src/app/ui-modules/ui-modules-home/ui-modules-home.component.ts
@@ -3,13 +3,15 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-ui-modules-home',
   templateUrl: './ui-modules-home.component.html',
-  styleUrls: ['./ui-modules-home.component.css']
+  styleUrls: ['./ui-modules-home.component.css'],
 })
 export class UiModulesHomeComponent implements OnInit {
+  openModal = false;
+  constructor() {}
 
-  constructor() { }
+  ngOnInit(): void {}
 
-  ngOnInit(): void {
+  onToggleModal() {
+    this.openModal = !this.openModal;
   }
-
 }
--- ui-widgets/src/app/ui-modules/ui-modules-modal/ui-modules-modal.component.ts
+++ ui-widgets/src/app/ui-modules/ui-modules-modal/ui-modules-modal.component.ts
@@ -1,15 +1,30 @@
-import { Component, OnInit } from '@angular/core';
+import {
+  Component,
+  OnInit,
+  OnDestroy,
+  ElementRef,
+  Output,
+  EventEmitter,
+} from '@angular/core';
 
 @Component({
   selector: 'app-ui-modules-modal',
   templateUrl: './ui-modules-modal.component.html',
-  styleUrls: ['./ui-modules-modal.component.css']
+  styleUrls: ['./ui-modules-modal.component.css'],
 })
-export class UiModulesModalComponent implements OnInit {
-
-  constructor() { }
+export class UiModulesModalComponent implements OnInit, OnDestroy {
+  @Output() close = new EventEmitter();
+  constructor(private el: ElementRef) {}
 
   ngOnInit(): void {
+    document.body.appendChild(this.el.nativeElement);
   }
 
+  ngOnDestroy(): void {
+    this.el.nativeElement.remove();
+  }
+
+  onCloseModal() {
+    this.close.emit();
+  }
 }
