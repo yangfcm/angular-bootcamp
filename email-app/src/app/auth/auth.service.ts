53e397586a61742b6bfb7ba4addb240ab01567fb
Fan Yang <yangfcm16@gmail.com>
2020-06-04 23:11:18 +0930

    email-app: add auth service

---
+++ email-app/src/app/auth/auth.service.ts
@@ -0,0 +1,19 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { environment } from '../../environments/environment';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class AuthService {
+  constructor(private http: HttpClient) {}
+
+  usernameAvailable(username: string) {
+    return this.http.post<{ available: boolean }>(
+      `${environment.apiUrl}/auth/username`,
+      {
+        username,
+      }
+    );
+  }
+}
--- email-app/src/app/auth/validators/unique-username.ts
+++ email-app/src/app/auth/validators/unique-username.ts
@@ -3,34 +3,29 @@ import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { map, catchError } from 'rxjs/operators';
 import { of } from 'rxjs';
-import { environment } from '../../../environments/environment';
+import { AuthService } from '../auth.service';
 
 @Injectable({
   providedIn: 'root',
 })
 export class UniqueUsername implements AsyncValidator {
-  constructor(private http: HttpClient) {}
+  constructor(private authService: AuthService) {}
 
   validate = (control: FormControl) => {
     const { value } = control;
-    console.log(this.http);
-    return this.http
-      .post<any>(`${environment.apiUrl}/auth/username`, {
-        username: value,
+    return this.authService.usernameAvailable(value).pipe(
+      map((value) => {
+        if (value.available) {
+          return null;
+        }
+      }),
+      catchError((err) => {
+        if (err.error.username) {
+          return of({ nonUniqueUsername: true });
+        } else {
+          return of({ error: err.message });
+        }
       })
-      .pipe(
-        map((value) => {
-          if (value.available) {
-            return null;
-          }
-        }),
-        catchError((err) => {
-          if (err.error.username) {
-            return of({ nonUniqueUsername: true });
-          } else {
-            return of({ error: err.message });
-          }
-        })
-      );
+    );
   };
 }
