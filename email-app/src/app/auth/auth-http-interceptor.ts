409259cd41db1fa2efeda854dc72585d5e356bc3
Fan Yang <yangfcm16@gmail.com>
2020-06-08 21:17:19 +0930

    email-app: set up http interceptor class

--- email-app/src/app/auth/auth-http-interceptor.ts
+++ email-app/src/app/auth/auth-http-interceptor.ts
@@ -1,2 +1,16 @@
-export class AuthHttpInterceptor {
+import { Injectable } from '@angular/core';
+import {
+  HttpEvent,
+  HttpInterceptor,
+  HttpHandler,
+  HttpRequest,
+} from '@angular/common/http';
+import { Observable } from 'rxjs';
+
+@Injectable()
+export class AuthHttpInterceptor implements HttpInterceptor {
+  intercept(
+    req: HttpRequest<any>,
+    next: HttpHandler
+  ): Observable<HttpEvent<any>> {}
 }
