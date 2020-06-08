20727493b88b04cd589d2435f233f93a68f3d162
Fan Yang <yangfcm16@gmail.com>
2020-06-08 20:54:15 +0930

    email-app: check signedIn status

--- email-app/src/app/app.component.ts
+++ email-app/src/app/app.component.ts
@@ -1,10 +1,20 @@
 import { Component } from '@angular/core';
+import { AuthService } from './auth/auth.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
   title = 'email-app';
+  signedIn = false;
+  constructor(private authService: AuthService) {}
+
+  ngOnInit() {
+    this.authService.signedIn$.subscribe((signedIn) => {
+      this.signedIn = signedIn;
+    });
+    this.authService.checkAuth().subscribe(() => {});
+  }
 }
--- email-app/src/app/auth/auth.service.ts
+++ email-app/src/app/auth/auth.service.ts
@@ -1,5 +1,7 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
+import { BehaviorSubject } from 'rxjs';
+import { tap } from 'rxjs/operators';
 import { environment } from '../../environments/environment';
 
 interface SignupCredentials {
@@ -17,6 +19,8 @@ interface SignupResponse {
 })
 export class AuthService {
   rootUrl = environment.apiUrl;
+  signedIn$ = new BehaviorSubject(false);
+
   constructor(private http: HttpClient) {}
 
   usernameAvailable(username: string) {
@@ -29,11 +33,35 @@ export class AuthService {
   }
 
   signup(credentials: SignupCredentials) {
-    console.log(credentials);
-    return this.http.post<SignupResponse>(`${this.rootUrl}/auth/signup`, {
-      username: credentials.username,
-      password: credentials.password,
-      passwordConfirmation: credentials.passwordConf,
-    });
+    // console.log(credentials);
+    return this.http
+      .post<SignupResponse>(
+        `${this.rootUrl}/auth/signup`,
+        {
+          username: credentials.username,
+          password: credentials.password,
+          passwordConfirmation: credentials.passwordConf,
+        },
+        {
+          withCredentials: true,
+        }
+      )
+      .pipe(
+        tap(() => {
+          this.signedIn$.next(true);
+        })
+      );
+  }
+
+  checkAuth() {
+    return this.http
+      .get(`${this.rootUrl}/auth/signedin`, {
+        withCredentials: true,
+      })
+      .pipe(
+        tap((response) => {
+          console.log(response);
+        })
+      );
   }
 }
