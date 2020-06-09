4096c039c83c20c5077d3537a9459c03a5dc6e1a
Fan Yang <yangfcm16@gmail.com>
2020-06-09 17:18:52 +0930

    email-app: show html

--- email-app/src/app/inbox/email-resolver.service.ts
+++ email-app/src/app/inbox/email-resolver.service.ts
@@ -1,5 +1,7 @@
 import { Injectable } from '@angular/core';
-import { Resolve, ActivatedRouteSnapshot } from '@angular/router';
+import { Resolve, ActivatedRouteSnapshot, Router } from '@angular/router';
+import { EMPTY } from 'rxjs';
+import { catchError } from 'rxjs/operators';
 import { IEmail } from './email.interface';
 import { EmailService } from './email.service';
 
@@ -7,10 +9,15 @@ import { EmailService } from './email.service';
   providedIn: 'root',
 })
 export class EmailResolverService implements Resolve<IEmail> {
-  constructor(private emailService: EmailService) {}
+  constructor(private emailService: EmailService, private router: Router) {}
 
   resolve(route: ActivatedRouteSnapshot) {
     const { id } = route.params;
-    return this.emailService.getEmail(id);
+    return this.emailService.getEmail(id).pipe(
+      catchError(() => {
+        this.router.navigateByUrl('/inbox/not-found');
+        return EMPTY;
+      })
+    );
   }
 }
