e0be7eac8a86d7a387f090ba4c00f7e870cd434d
Fan Yang <yangfcm16@gmail.com>
2020-06-10 12:08:55 +0930

    email-app: add modal

--- email-app/src/app/inbox/email-create/email-create.component.ts
+++ email-app/src/app/inbox/email-create/email-create.component.ts
@@ -3,13 +3,11 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-email-create',
   templateUrl: './email-create.component.html',
-  styleUrls: ['./email-create.component.css']
+  styleUrls: ['./email-create.component.css'],
 })
 export class EmailCreateComponent implements OnInit {
+  showModal = false;
+  constructor() {}
 
-  constructor() { }
-
-  ngOnInit(): void {
-  }
-
+  ngOnInit(): void {}
 }
---
+++ email-app/src/app/shared/modal/modal.component.ts
@@ -0,0 +1,30 @@
+import {
+  Component,
+  OnInit,
+  ElementRef,
+  Output,
+  OnDestroy,
+  EventEmitter,
+} from '@angular/core';
+
+@Component({
+  selector: 'app-modal',
+  templateUrl: './modal.component.html',
+  styleUrls: ['./modal.component.css'],
+})
+export class ModalComponent implements OnInit, OnDestroy {
+  @Output() dismiss = new EventEmitter();
+  constructor(private el: ElementRef) {}
+
+  ngOnInit(): void {
+    document.body.appendChild(this.el.nativeElement);
+  }
+
+  ngOnDestroy(): void {
+    this.el.nativeElement.remove();
+  }
+
+  onDismissClick() {
+    this.dismiss.emit();
+  }
+}
