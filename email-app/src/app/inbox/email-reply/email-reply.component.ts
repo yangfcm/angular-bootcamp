3955cffc8895f9847ba6b8be1c1242c1c02a541e
Fan Yang <yangfcm16@gmail.com>
2020-06-10 23:13:07 +0930

    email-app: implement reply email

--- email-app/src/app/inbox/email-reply/email-reply.component.ts
+++ email-app/src/app/inbox/email-reply/email-reply.component.ts
@@ -1,15 +1,31 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input, OnChanges } from '@angular/core';
+import { IEmail } from '../email.interface';
+import { EmailService } from '../email.service';
 
 @Component({
   selector: 'app-email-reply',
   templateUrl: './email-reply.component.html',
-  styleUrls: ['./email-reply.component.css']
+  styleUrls: ['./email-reply.component.css'],
 })
-export class EmailReplyComponent implements OnInit {
+export class EmailReplyComponent implements OnInit, OnChanges {
+  showModal = false;
+  @Input() email: IEmail;
 
-  constructor() { }
-
-  ngOnInit(): void {
+  constructor(private emailService: EmailService) {}
+  ngOnInit(): void {}
+  ngOnChanges(): void {
+    this.email = {
+      ...this.email,
+      from: this.email.to,
+      to: this.email.from,
+      subject: `RE: ${this.email.subject}`,
+      text: `\n\n\n------------- ${this.email.from} wrote:\n>${this.email.text}`,
+    };
   }
 
+  onSendEmail(email: IEmail) {
+    this.emailService.sendEmail(email).subscribe(() => {
+      this.showModal = false;
+    });
+  }
 }
