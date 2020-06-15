47b40a6b89398ff690a8968352c05dc69f2dbbbc
Fan Yang <yangfcm16@gmail.com>
2020-06-15 23:42:24 +0930

    landing: design notifications service

--- landing/src/app/notifications/notifications.service.ts
+++ landing/src/app/notifications/notifications.service.ts
@@ -1,9 +1,62 @@
 import { Injectable } from '@angular/core';
+import { Subject, Observable } from 'rxjs';
+import { scan } from 'rxjs/operators';
+
+enum CommandType {
+  success = 'success',
+  error = 'error',
+  clear = 'clear',
+}
+
+interface ICommand {
+  id: number;
+  type: CommandType;
+  text?: string;
+}
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class NotificationsService {
+  messagesInput: Subject<ICommand>;
+  messagesOutput: Observable<ICommand[]>;
+  constructor() {
+    this.messagesInput = new Subject<ICommand>();
+    this.messagesOutput = this.messagesInput.pipe(
+      scan((acc: ICommand[], value: ICommand) => {
+        if (value.type === CommandType.clear) {
+          return acc.filter((message) => message.id !== value.id);
+        } else {
+          return [...acc, value];
+        }
+      }, [])
+    );
+  }
 
-  constructor() { }
+  addSuccess(message: string) {
+    this.messagesInput.next({
+      id: this.randomId(),
+      type: CommandType.success,
+      text: message,
+    });
+  }
+
+  addError(message: string) {
+    this.messagesInput.next({
+      id: this.randomId(),
+      type: CommandType.error,
+      text: message,
+    });
+  }
+
+  clearMessage(id: number) {
+    this.messagesInput.next({
+      id,
+      type: CommandType.clear,
+    });
+  }
+
+  private randomId() {
+    return Math.round(Math.random() * 10000);
+  }
 }
