e699473ab780a58c3c15b6b356bee634c209fdcc
Fan Yang <yangfcm16@gmail.com>
2021-07-06 00:35:19 +0930

    pipes: update readme and clear test.

--- pipes/src/app/app.component.spec.ts
+++ pipes/src/app/app.component.spec.ts
@@ -1,10 +1,11 @@
 import { TestBed, async } from '@angular/core/testing';
 import { AppComponent } from './app.component';
+import { ConvertPipe } from './convert.pipe';
 
 describe('AppComponent', () => {
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [AppComponent],
+      declarations: [AppComponent, ConvertPipe],
     }).compileComponents();
   }));
 
@@ -12,20 +13,5 @@ describe('AppComponent', () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
     expect(app).toBeTruthy();
-  });
-
-  it(`should have as title 'pipes'`, () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    const app = fixture.componentInstance;
-    expect(app.title).toEqual('pipes');
-  });
-
-  it('should render title', () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    fixture.detectChanges();
-    const compiled = fixture.nativeElement;
-    expect(compiled.querySelector('.content span').textContent).toContain(
-      'pipes app is running!'
-    );
   });
 });
