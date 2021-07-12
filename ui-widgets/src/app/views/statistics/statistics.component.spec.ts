6ccdedb3e255a39ec3df4571134c932faec9776f
Fan Yang <yangfcm16@gmail.com>
2021-07-13 00:23:12 +0930

    ui-widgets: test statistics component.

--- ui-widgets/src/app/views/statistics/statistics.component.spec.ts
+++ ui-widgets/src/app/views/statistics/statistics.component.spec.ts
@@ -1,25 +1,43 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
-
+import { DebugElement } from '@angular/core';
+import { By } from '@angular/platform-browser';
 import { StatisticsComponent } from './statistics.component';
+import { statsData } from '../views-home/views-home.component';
 
 describe('StatisticsComponent', () => {
   let component: StatisticsComponent;
   let fixture: ComponentFixture<StatisticsComponent>;
+  let statValues: DebugElement[], statLabels: DebugElement[];
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [ StatisticsComponent ]
-    })
-    .compileComponents();
+      declarations: [StatisticsComponent],
+    }).compileComponents();
   }));
 
   beforeEach(() => {
     fixture = TestBed.createComponent(StatisticsComponent);
     component = fixture.componentInstance;
+    component.data = statsData;
     fixture.detectChanges();
+    statValues = fixture.debugElement.queryAll(By.css('.statistic .value'));
+    statLabels = fixture.debugElement.queryAll(By.css('.statistic .label'));
   });
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('should render correct statistics values and labels', () => {
+    expect(statValues.length).toBe(statsData.length);
+    expect(statLabels.length).toBe(statsData.length);
+    for (let i = 0; i < statsData.length; i++) {
+      expect(statValues[i].nativeElement.innerText).toContain(
+        statsData[i].value
+      );
+      expect(statLabels[i].nativeElement.innerText).toContain(
+        statsData[i].label.toUpperCase()
+      );
+    }
   });
 });
