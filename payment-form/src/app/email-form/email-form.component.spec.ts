af087fd423f23e01396baec719156a166a16c7d9
Fan Yang <yangfcm16@gmail.com>
2021-07-17 14:39:48 +0930

    test email form component.

--- payment-form/src/app/email-form/email-form.component.spec.ts
+++ payment-form/src/app/email-form/email-form.component.spec.ts
@@ -1,12 +1,6 @@
 import { DebugElement } from '@angular/core';
-import {
-  async,
-  ComponentFixture,
-  fakeAsync,
-  TestBed,
-  tick,
-} from '@angular/core/testing';
-import { FormsModule, NgModel } from '@angular/forms';
+import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { FormsModule } from '@angular/forms';
 import { By } from '@angular/platform-browser';
 
 import { EmailFormComponent } from './email-form.component';
@@ -33,7 +27,7 @@ describe('EmailFormComponent', () => {
     expect(component).toBeTruthy();
   });
 
-  it('should have invalid form and disabled submit button', async(() => {
+  it('should have invalid form and disabled submit button initally', async(() => {
     fixture.whenStable().then(() => {
       const email = component.emailForm.form.controls['email'];
       expect(email.invalid).toBeTruthy();
@@ -68,6 +62,67 @@ describe('EmailFormComponent', () => {
       expect(errorEl.nativeElement.innerText).not.toContain(
         'Email is required'
       );
+    });
+  }));
+
+  it('should render correct error message and disable submit button if email is not provided', async(() => {
+    fixture.whenStable().then(() => {
+      emailInput.triggerEventHandler('input', {
+        target: {
+          value: '',
+        },
+      });
+      const email = component.emailForm.form.controls['email'];
+      email.markAsTouched();
+      expect(email.invalid).toBeTruthy();
+      expect(component.emailForm.invalid).toBeTruthy();
+
+      fixture.detectChanges();
+      submitBtn = fixture.debugElement.query(By.css('button.button'));
+      expect(submitBtn.nativeElement.disabled).toBeTruthy();
+
+      const errorEl = fixture.debugElement.query(By.css('#email-error'));
+      expect(errorEl.nativeElement.innerText).not.toContain(
+        'Email provided is invalid'
+      );
+      expect(errorEl.nativeElement.innerText).toContain('Email is required');
+    });
+  }));
+
+  it('should enable submit button and no error message is valid email is provided', async(() => {
+    fixture.whenStable().then(() => {
+      emailInput.triggerEventHandler('input', {
+        target: {
+          value: 'test@test.com',
+        },
+      });
+      const email = component.emailForm.form.controls['email'];
+      email.markAsTouched();
+      expect(email.valid).toBeTruthy();
+      expect(component.emailForm.valid).toBeTruthy();
+
+      fixture.detectChanges();
+      submitBtn = fixture.debugElement.query(By.css('button.button'));
+      expect(submitBtn.nativeElement.disabled).toBeFalsy();
+
+      const errorEl = fixture.debugElement.query(By.css('#email-error'));
+      expect(errorEl).toBeFalsy();
+    });
+  }));
+
+  it('should be able to submit form with the input value', async(() => {
+    fixture.whenStable().then(() => {
+      component.onSubmitForm = jasmine.createSpy('onSubmitFormSpy');
+      emailInput.triggerEventHandler('input', {
+        target: {
+          value: 'test@test.com',
+        },
+      });
+      const email = component.emailForm.form.controls['email'];
+      email.markAsTouched();
+      form.triggerEventHandler('submit', null);
+      expect(component.email).toBe('test@test.com');
+      expect(component.onSubmitForm).toHaveBeenCalled();
     });
   }));
 });
