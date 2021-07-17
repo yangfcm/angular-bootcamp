483cce6ac9503941470438ff0e61ff8dc95068b5
Fan Yang <yangfcm16@gmail.com>
2021-07-17 22:25:40 +0930

    test answer-highlight directive and math-validators.

--- maths-form/src/app/answer-highlight.directive.spec.ts
+++ maths-form/src/app/answer-highlight.directive.spec.ts
@@ -1,8 +1,70 @@
+import { Component, DebugElement } from '@angular/core';
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { FormGroup, FormControl, ReactiveFormsModule } from '@angular/forms';
+import { By } from '@angular/platform-browser';
 import { AnswerHighlightDirective } from './answer-highlight.directive';
 
+@Component({
+  template: ` <form [formGroup]="mathForm">
+    <div class="equation">{{ number1 }} + {{ number2 }}</div>
+    <input formControlName="answer" appAnswerHighlight />
+  </form>`,
+})
+class TestComponent {
+  mathForm = new FormGroup({
+    number1: new FormControl(3),
+    number2: new FormControl(5),
+    answer: new FormControl(''),
+  });
+}
+
 describe('AnswerHighlightDirective', () => {
-  it('should create an instance', () => {
-    // const directive = new AnswerHighlightDirective();
-    // expect(directive).toBeTruthy();
+  let fixture: ComponentFixture<TestComponent>;
+  let component: TestComponent;
+  let answerInput: DebugElement;
+
+  beforeEach(() => {
+    fixture = TestBed.configureTestingModule({
+      declarations: [TestComponent, AnswerHighlightDirective],
+      imports: [ReactiveFormsModule],
+    }).createComponent(TestComponent);
+    component = fixture.componentInstance;
+    answerInput = fixture.debugElement.query(
+      By.directive(AnswerHighlightDirective)
+    );
+    fixture.detectChanges();
+  });
+
+  it('should have correct class applied to answer if answer = number1 + number2', () => {
+    answerInput.triggerEventHandler('input', {
+      target: {
+        value: 8,
+      },
+    });
+    fixture.detectChanges();
+    expect(answerInput.nativeElement.className).toContain('correct');
+    expect(answerInput.nativeElement.className).not.toContain('close');
+  });
+
+  it('should have close class applied to answer if answer is not equal to but close to number1 + number2', () => {
+    answerInput.triggerEventHandler('input', {
+      target: {
+        value: 9,
+      },
+    });
+    fixture.detectChanges();
+    expect(answerInput.nativeElement.className).toContain('close');
+    expect(answerInput.nativeElement.className).not.toContain('correct');
+  });
+
+  it('should not have correct or close class if answer is far from number1 + number2', () => {
+    answerInput.triggerEventHandler('input', {
+      target: {
+        value: 99,
+      },
+    });
+    fixture.detectChanges();
+    expect(answerInput.nativeElement.className).not.toContain('close');
+    expect(answerInput.nativeElement.className).not.toContain('correct');
   });
 });
--- maths-form/src/app/math-validators.spec.ts
+++ maths-form/src/app/math-validators.spec.ts
@@ -1,7 +1,34 @@
 import { MathValidators } from './math-validators';
+import { FormControl, FormGroup } from '@angular/forms';
 
 describe('MathValidators', () => {
   it('should create an instance', () => {
     expect(new MathValidators()).toBeTruthy();
+  });
+
+  it('should have static method named addition', () => {
+    expect(MathValidators.addition).toBeTruthy();
+  });
+
+  it('should return null if answer = number1 + number2', () => {
+    const validateFn = MathValidators.addition('number1', 'number2', 'answer');
+    const form = new FormGroup({
+      number1: new FormControl(3),
+      number2: new FormControl(5),
+      answer: new FormControl(8),
+    });
+    expect(validateFn(form)).toBeNull();
+  });
+
+  it('should return error information if answer not equal number1 + number2', () => {
+    const validateFn = MathValidators.addition('number1', 'number2', 'answer');
+    const form = new FormGroup({
+      number1: new FormControl(3),
+      number2: new FormControl(5),
+      answer: new FormControl(9),
+    });
+    expect(validateFn(form)).toEqual({
+      addition: true,
+    });
   });
 });
