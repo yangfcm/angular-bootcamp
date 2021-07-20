d524fb42c5f2e0c3e1bb98140bb791479c22e86d
Fan Yang <yangfcm16@gmail.com>
2021-07-21 00:48:44 +0930

    testing input component.

--- email-app/src/app/shared/input/input.component.spec.ts
+++ email-app/src/app/shared/input/input.component.spec.ts
@@ -1,25 +1,58 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { DebugElement } from '@angular/core';
+import { FormControl, ReactiveFormsModule, Validators } from '@angular/forms';
+import { By } from '@angular/platform-browser';
 
 import { InputComponent } from './input.component';
 
 describe('InputComponent', () => {
   let component: InputComponent;
   let fixture: ComponentFixture<InputComponent>;
+  let input: DebugElement, error: DebugElement;
 
-  // beforeEach(async(() => {
-  //   TestBed.configureTestingModule({
-  //     declarations: [ InputComponent ]
-  //   })
-  //   .compileComponents();
-  // }));
+  function basicSetting() {
+    TestBed.configureTestingModule({
+      declarations: [InputComponent],
+      imports: [ReactiveFormsModule],
+    }).compileComponents();
+    fixture = TestBed.createComponent(InputComponent);
+    component = fixture.componentInstance;
+  }
 
-  // beforeEach(() => {
-  //   fixture = TestBed.createComponent(InputComponent);
-  //   component = fixture.componentInstance;
-  //   fixture.detectChanges();
-  // });
+  describe('Basic Text InputComponent', () => {
+    beforeEach(() => {
+      basicSetting();
+      component.label = 'username';
+      component.type = 'text';
+      component.control = new FormControl('user', [
+        Validators.required,
+        Validators.minLength(3),
+        Validators.maxLength(10),
+        Validators.pattern(/^[a-zA-Z0-9]+$/),
+      ]);
 
-  // it('should create', () => {
-  //   expect(component).toBeTruthy();
-  // });
+      input = fixture.debugElement.query(By.css('input'));
+      fixture.detectChanges();
+    });
+
+    it('should create', () => {
+      expect(component).toBeTruthy();
+    });
+  });
+
+  describe('Basic Textarea IinputComponent', () => {
+    beforeEach(() => {
+      basicSetting();
+      component.label = 'content';
+      component.controlType = 'textarea';
+      component.control = new FormControl('...');
+
+      input = fixture.debugElement.query(By.css('textarea'));
+      fixture.detectChanges();
+    });
+
+    it('should create', () => {
+      expect(component).toBeTruthy();
+    });
+  });
 });
