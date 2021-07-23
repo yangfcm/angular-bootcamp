f9a8df014d424d2b0e949e75e3030060ca7a3f22
Fan Yang <yangfcm16@gmail.com>
2021-07-23 23:43:03 +0930

    email-app: start testing email service.

--- email-app/src/app/inbox/email.service.spec.ts
+++ email-app/src/app/inbox/email.service.spec.ts
@@ -1,16 +1,39 @@
 import { TestBed } from '@angular/core/testing';
+import {
+  HttpClientTestingModule,
+  HttpTestingController,
+} from '@angular/common/http/testing';
+import { HTTP_INTERCEPTORS } from '@angular/common/http';
+import { AuthHttpInterceptor } from '../auth/auth-http-interceptor';
 
 import { EmailService } from './email.service';
 
 describe('EmailService', () => {
   let service: EmailService;
+  let httpTestingController: HttpTestingController;
 
-  // beforeEach(() => {
-  //   TestBed.configureTestingModule({});
-  //   service = TestBed.inject(EmailService);
-  // });
+  beforeEach(() => {
+    TestBed.configureTestingModule({
+      imports: [HttpClientTestingModule],
+      providers: [
+        {
+          provide: HTTP_INTERCEPTORS,
+          useClass: AuthHttpInterceptor,
+          multi: true,
+        },
+      ],
+    });
+    httpTestingController = TestBed.inject(HttpTestingController);
+    service = TestBed.inject(EmailService);
+  });
 
-  // it('should be created', () => {
-  //   expect(service).toBeTruthy();
-  // });
+  it('should be created', () => {
+    expect(service).toBeTruthy();
+  });
+
+  it('should get emails');
+
+  it('should get one email by id');
+
+  it('should send email');
 });
